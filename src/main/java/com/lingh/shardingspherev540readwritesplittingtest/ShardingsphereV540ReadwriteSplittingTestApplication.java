a75c56083948692cc4df3963e2c9cb9a72e8dec9
linghengqian <linghengqian@outlook.com>
2023-08-01 21:46:31 +0800

    update project for test

---
+++ src/main/java/com/lingh/shardingspherev540readwritesplittingtest/ShardingsphereV540ReadwriteSplittingTestApplication.java
@@ -0,0 +1,13 @@
+package com.lingh.shardingspherev540readwritesplittingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingsphereV540ReadwriteSplittingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingsphereV540ReadwriteSplittingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lingh/shardingspherev540readwritesplittingtest/ShardingsphereV540ReadwriteSplittingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingh.shardingspherev540readwritesplittingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingsphereV540ReadwriteSplittingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
